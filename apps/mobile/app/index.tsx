275f84082698ae3dd2ace5d4fd1a197f3e36b6cd
Jason Francois <jasonc.francois@gmail.com>
2024-01-12 14:35:50 -0500

    Removed uneccessary template code

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+const Home = () => {
+  return (
+    <View>
+      <Text>Welcome to Staffing for Nurses!</Text>
+    </View>
+  );
+};
+
+export default Home;
