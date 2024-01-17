f0b0a26936971c522dec8ad36a349ad98f97c61f
Jason Francois <jasonc.francois@gmail.com>
2024-01-17 07:37:27 -0500

    added file upload controller

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,17 +1,25 @@
-import { Express, Request, Response } from 'express';
+import { Express, Request, Response } from "express";
 
-const express = require('express')
-const dotenv = require('dotenv');
-dotenv.config()
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app: Express = express();
 
-const app: Express = express()
+const upload = require("./controllers/upload");
+
+dotenv.config();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use("/upload", upload);
+
 const port = process.env.PORT || 3000;
 
-
-app.get('/', (req: Request, res: Response): void => {
-  res.send('Hello World!')
-})
+app.get("/", (req: Request, res: Response): void => {
+  res.send("Hello World!");
+});
 
 app.listen(port, (): void => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
