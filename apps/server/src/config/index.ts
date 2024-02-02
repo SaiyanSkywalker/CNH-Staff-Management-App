4fdb8669cf128f7a14f61dfad066c6c766fc3494
Jason Francois <jasonc.francois@gmail.com>
2024-02-01 23:05:31 -0500

    Changes to allow for oracle sql connections

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -7,7 +7,6 @@ export default {
   db: process.env.DB || "cnh_staff_management",
   db_user: process.env.DB_USER || "root",
   db_password: process.env.DB_PASSWORD || "password",
-  db_hostname: process.env.DB_HOSTNAME || "hostname",
-  db_dialect: process.env.DB_DIALECT || "mysql",
-  db_port: process.env.DB_PORT || "3306",
+  db_dialect: process.env.DB_DIALECT || "oracle",
+  db_connection_string: process.env.DB_CONNECTION_STRING || "connectionString",
 };
--- apps/server/src/loaders/dbLoader.ts
+++ apps/server/src/loaders/dbLoader.ts
@@ -3,22 +3,18 @@ import createModels from "../models";
 
 let sequelize: Sequelize;
 
-const fs = require("fs");
-const path = require("path");
 export default async (config: any) => {
   try {
     const dbConfig = {
       username: config.db_user,
       password: config.db_password,
-      host: config.db_hostname,
-      port: config.db_port,
       dialect: config.db_dialect,
+      dialectOptions: { connectString: config.db_connection_string },
     };
     sequelize = new Sequelize(dbConfig);
 
+    // Start the db and create initial models
     createModels(sequelize);
-
-    console.log(dbConfig);
     await sequelize.authenticate();
     await sequelize.sync();
 
