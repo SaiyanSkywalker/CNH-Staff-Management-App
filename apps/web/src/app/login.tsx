2c72a3228a81bd9763f9a7216ac820fe11413635
kerrikenn <kk3358@drexel.edu>
2024-02-05 02:08:15 -0500

    Added basic login UI and functionality

--- apps/web/src/app/login.tsx
+++ apps/web/src/app/login.tsx
@@ -1,6 +1,5 @@
-"use client"
 import Image from 'next/image'
-import { useRef, useState } from 'react'
+import { useRef, useState } from 'react';
 
 export default function Home() {
     const [username, setUsername] = useState("");
@@ -31,15 +30,11 @@ export default function Home() {
                 <div className="login-container">
                     <h2>Welcome!</h2>
                     <form>
-                        <label htmlFor="username">
-                            Username:
-                            <input type="text" onChange={onUsernameChange} placeholder="username" required />
-                        </label>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" onChange={onUsernameChange} name="username" required />
 
-                        <label htmlFor="password">
-                            Password:
-                            <input type="password" onChange={onPasswordChange} placeholder="password" required />
-                        </label>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" onChange={onPasswordChange} name="password" required />
 
                         <button type="button" onClick={submitForm}>Login</button>
                     </form>
