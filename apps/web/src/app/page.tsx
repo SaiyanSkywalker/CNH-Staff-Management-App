cb1b58256fee2379b5308286ce29905e99c89049
Jason Francois <jasonc.francois@gmail.com>
2024-02-06 20:54:50 -0500

    did some refactoring of the web project

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,50 +1,9 @@
-"use client"
-import { useRef, useState } from 'react'
-
-export default function Home() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const onUsernameChange = (e) => {
-        setUsername(e.target.value);
-    }
-
-    const onPasswordChange = (e) => {
-        setPassword(e.target.value);
-    }
-
-    const submitForm = () => {
-        // Perform validation and authentication logic here
-        if (username === 'example' && password === 'password') {
-            alert('Login successful!');
-            // Redirect or perform other actions after successful login
-        }
-        else {
-            alert('Invalid username or password. Please try again.');
-        }
-    }
-
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>
-                <div className="login-container">
-                    <h2 className="home-header">Welcome!</h2>
-                    <form>
-                        <div className="inputs-container">
-                            <label htmlFor="username">
-                                Username:
-                            <input type="text" onChange={onUsernameChange} placeholder="username" required />
-                            </label>
-
-                            <label htmlFor="password">
-                                Password:
-                            <input type="password" onChange={onPasswordChange} placeholder="password" required />
-                            </label>
-                        </div>
-                        <button type="button" onClick={submitForm}>Login</button>
-                    </form>
-                </div>
-            </div>
-        </main>
-    )
+"use client";
+import Login from "../components/Login";
+export default function Page() {
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,8 +1,5 @@
-
 // Once authentication is set up we can update the nav bar to show user links
 const Navbar = () => {
-    return (
-        <div className="navbar"></div>
-    )
-}
-export default Navbar
+  return <div className="navbar w-100 bg-[#067496] h-20"></div>;
+};
+export default Navbar;
