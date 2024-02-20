178420e44087304137b321668d5cdf108622a781
Jason Francois <jasonc.francois@gmail.com>
2024-02-20 18:39:53 -0500

    Added redirect to login screen on app start

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import React from "react";
+
 const Layout = () => {
   return <Stack />;
 };
--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,90 +1,11 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#71B7C7",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    textAlign: "center",
-    fontWeight: "bold",
-    fontSize: 36,
-    color: "#FFFFFF",
-    marginBottom: 40,
-  },
-  inputView: {
-    width: "80%",
-    backgroundColor: "#FFFFFF",
-    borderRadius: 25,
-    height: 50,
-    marginBottom: 20,
-    justifyContent: "center",
-    padding: 20,
-  },
-  inputText: {
-    height: 50,
-    color: "black",
-  },
-  forgotText: {
-    color: "white",
-    fontSize: 11,
-  },
-  loginBtn: {
-    width: "80%",
-    backgroundColor: "#fb5b5a",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    marginBottom: 10,
-  },
-});
+import { Redirect } from "expo-router";
+import React from "react";
+
 const Home = () => {
-  const onPressLogin = () => {
-    // Do something about login operation
-  };
-  const onPressForgotPassword = () => {
-    // Do something about forgot password operation
-  };
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}> Staffing for Nurses</Text>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.inputText}
-          placeholder="Email"
-          placeholderTextColor="#003f5c"
-          onChangeText={(text) => setEmail(text)}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.inputText}
-          secureTextEntry
-          placeholder="Password"
-          placeholderTextColor="#003f5c"
-          onChangeText={(text) => setPassword(text)}
-        />
-      </View>
-      <TouchableOpacity onPress={onPressForgotPassword}>
-        <Text style={styles.forgotText}>Forgot Password?</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onPressLogin} style={styles.loginBtn}>
-        <Text style={styles.forgotText}>LOGIN </Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      <Redirect href={`/login`} />
+    </>
   );
 };
-
 export default Home;
