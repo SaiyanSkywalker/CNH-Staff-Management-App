3deaf1581c6885a6d24d5a7edf53d8bfe02dba92
Jason Francois <jasonc.francois@gmail.com>
2024-02-23 19:52:17 -0500

    Added drawer to mobile app

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,7 +1,23 @@
-import { Stack } from "expo-router";
+import { DrawerToggleButton } from "@react-navigation/drawer";
+import { Drawer } from "expo-router/drawer";
 import React from "react";
 
 const Layout = () => {
-  return <Stack />;
+  return (
+    <Drawer
+      screenOptions={{
+        headerTitle: "",
+        headerLeft: () => <DrawerToggleButton />,
+      }}
+    >
+      <Drawer.Screen
+        name="index"
+        options={{
+          drawerLabel: "Home",
+          title: "overview",
+        }}
+      />
+    </Drawer>
+  );
 };
 export default Layout;
