06bb5c6e6e51f79300f611165ad5b28b2a7e563a
Jason Francois <jasonc.francois@gmail.com>
2024-02-23 23:04:39 -0500

    Added custom drawer button

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,20 +1,33 @@
-import { DrawerToggleButton } from "@react-navigation/drawer";
 import { Drawer } from "expo-router/drawer";
 import React from "react";
-
+import { Ionicons } from "@expo/vector-icons";
 const Layout = () => {
   return (
     <Drawer
-      screenOptions={{
-        headerTitle: "",
-        headerLeft: () => <DrawerToggleButton />,
-      }}
+      screenOptions={({ navigation }) => ({
+        headerLeft: () => (
+          <Ionicons
+            name="md-menu"
+            size={32}
+            color="black"
+            style={{ marginLeft: 16 }}
+            onPress={navigation.toggleDrawer}
+          />
+        ),
+      })}
     >
       <Drawer.Screen
         name="index"
         options={{
           drawerLabel: "Home",
-          title: "overview",
+          title: "Home",
+        }}
+      />
+      <Drawer.Screen
+        name="login"
+        options={{
+          drawerLabel: "Login",
+          title: "Login",
         }}
       />
     </Drawer>
