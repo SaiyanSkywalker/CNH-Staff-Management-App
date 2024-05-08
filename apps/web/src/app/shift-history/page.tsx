1c51143f37d352079c393d72b4636358d49d334a
Jason Francois <jasonc.francois@gmail.com>
2024-05-07 22:31:55 -0400

    bug fixes`

--- apps/web/src/app/shift-history/page.tsx
+++ apps/web/src/app/shift-history/page.tsx
@@ -4,18 +4,15 @@ import page from "@webSrc/styles/ShiftHistory.module.css";
 import { useAuth } from "@webSrc/contexts/AuthContext";
 import { useSearchParams } from "next/navigation";
 import ShiftHistoryClient from "@shared/src/interfaces/ShiftHistoryClient";
-
+import AdminShiftRequestUpdate from "@shared/src/interfaces/AdminShiftRequestUpdate";
 export default function shiftHistory() {
   const params = useSearchParams();
-  const employeeNameQuery: string = params.get('employeeName') ?? "";
-  const employeeIdQuery: string = params.get('employeeId') ?? "";
-  const unitQuery: string = params.get('unit') ?? "";
-  const dateQuery: string = params.get('date') ?? "";
-  const shiftQuery: string = params.get('shift') ?? "";
-  const statusQuery: string = params.get('status') ?? "";
-
-  console.log(`employeeNameQuery is ${employeeNameQuery}, employeeIdQuery is ${employeeIdQuery}, unitQuery is ${unitQuery}, dateQuery is ${dateQuery}, shiftQuery is ${shiftQuery}, statusQuery is ${statusQuery}`);
-
+  const employeeNameQuery: string = params.get("employeeName") ?? "";
+  const employeeIdQuery: string = params.get("employeeId") ?? "";
+  const unitQuery: string = params.get("unit") ?? "";
+  const dateQuery: string = params.get("date") ?? "";
+  const shiftQuery: string = params.get("shift") ?? "";
+  const statusQuery: string = params.get("status") ?? "";
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [employeeId, setEmployeeId] = useState<string>(employeeIdQuery);
   const [employeeName, setEmployeeName] = useState<string>(employeeNameQuery);
@@ -23,7 +20,9 @@ export default function shiftHistory() {
   const [date, setDate] = useState<string>(dateQuery);
   const [shift, setShift] = useState<string>(shiftQuery);
   const [status, setStatus] = useState<string>(statusQuery);
-  const [shiftHistories, setShiftHistories] = useState<ShiftHistoryClient[]>([]);
+  const [shiftHistories, setShiftHistories] = useState<ShiftHistoryClient[]>(
+    []
+  );
   const { auth } = useAuth();
 
   const validateEmployeeId = (): boolean => {
@@ -58,13 +57,9 @@ export default function shiftHistory() {
         },
       }
     );
-    console.log("res is:");
-    console.dir(res);
     const jsonResponse: ShiftHistoryClient[] = await res.json();
-    console.log("jsonResponse is:");
-    console.dir(jsonResponse);
     setIsLoading((prevLoad) => !prevLoad);
-    setShiftHistories((prevshiftHistories) => jsonResponse);
+    setShiftHistories(jsonResponse);
   };
 
   const handleEmployeeIdChange = (event: BaseSyntheticEvent): void => {
@@ -114,16 +109,33 @@ export default function shiftHistory() {
     };
   };
 
-
-  const acceptOrDenyShift = (shiftHistoryId: number, isAccepted: boolean): void => {
-    //console.log("shiftHistoryId is:", shiftHistoryId);
-    //console.log("isAccepted is:", isAccepted);
+  const acceptOrDenyShift = (
+    shiftHistoryId: number,
+    isAccepted: boolean
+  ): void => {
     auth?.socket?.emit("shift_accept", {
       shiftHistoryId,
-      isAccepted
+      isAccepted,
     });
-  }
+  };
 
+  // Updates the UI based on if shift is accepted or rejected (based on socket response)
+  const shiftRequestResponseHandler = (sh: AdminShiftRequestUpdate) => {
+    const newShiftHistories = shiftHistories.map(
+      (shiftHistory: ShiftHistoryClient) => {
+        if (shiftHistory.id === sh.shiftHistory.id) {
+          return {
+            ...shiftHistory,
+            status: sh.isAccepted ? "Accepted" : "Rejected",
+          };
+        }
+        return shiftHistory;
+      }
+    );
+    if (newShiftHistories.length > 0) {
+      setShiftHistories(newShiftHistories);
+    }
+  };
   // Initial fill of list
   useEffect(() => {
     async function initialList() {
@@ -132,6 +144,11 @@ export default function shiftHistory() {
     initialList();
   }, []);
 
+  useEffect(() => {
+    if (auth?.socket && shiftHistories.length > 0) {
+      auth?.socket.on("shift_accept_response", shiftRequestResponseHandler);
+    }
+  }, [auth, shiftHistories]);
   return (
     <>
       <div className="w-full">
@@ -139,64 +156,63 @@ export default function shiftHistory() {
         <form className={page.form} onSubmit={getShiftHistories}>
           <div className={page.submission}>
             <div>
-            <label className={page.label}>Employee ID</label>
-            <input
-              placeholder="Enter ID"
-              value={employeeId}
-              onChange={handleEmployeeIdChange}
-              className={page.input}
-            ></input>
+              <label className={page.label}>Employee ID</label>
+              <input
+                placeholder="Enter ID"
+                value={employeeId}
+                onChange={handleEmployeeIdChange}
+                className={page.input}
+              ></input>
             </div>
             <div>
-            <label className={page.label}>Employee Name</label>
-            <input
-              placeholder="Enter Name"
-              value={employeeName}
-              onChange={handleEmployeeNameChange}
-              className={page.input}
-            ></input>
+              <label className={page.label}>Employee Name</label>
+              <input
+                placeholder="Enter Name"
+                value={employeeName}
+                onChange={handleEmployeeNameChange}
+                className={page.input}
+              ></input>
             </div>
 
             <div>
-            <label className={page.label}>Unit</label>
-            <input
-              placeholder="Enter Unit"
-              value={unit}
-              onChange={handleUnitChange}
-              className={page.input}
-            ></input>
+              <label className={page.label}>Unit</label>
+              <input
+                placeholder="Enter Unit"
+                value={unit}
+                onChange={handleUnitChange}
+                className={page.input}
+              ></input>
             </div>
 
             <div>
-            <label className={page.label}>Date</label>
-            <input
-              placeholder="Enter Date"
-              value={date}
-              onChange={handleDateChange}
-              className={page.input}
-            ></input>
+              <label className={page.label}>Date</label>
+              <input
+                placeholder="Enter Date"
+                value={date}
+                onChange={handleDateChange}
+                className={page.input}
+              ></input>
             </div>
 
             <div>
-            <label className={page.label}>Shift</label>
-            <input
-              placeholder="Enter Shift"
-              value={shift}
-              onChange={handleShiftChange}
-              className={page.input}
-            ></input>
+              <label className={page.label}>Shift</label>
+              <input
+                placeholder="Enter Shift"
+                value={shift}
+                onChange={handleShiftChange}
+                className={page.input}
+              ></input>
             </div>
 
             <div>
-            <label className={page.label}>Status</label>
-            <input
-              placeholder="Enter Status"
-              value={status}
-              onChange={handleStatusChange}
-              className={page.input}
-            ></input>
+              <label className={page.label}>Status</label>
+              <input
+                placeholder="Enter Status"
+                value={status}
+                onChange={handleStatusChange}
+                className={page.input}
+              ></input>
             </div>
-
           </div>
           <button
             className={page.button}
@@ -227,24 +243,39 @@ export default function shiftHistory() {
                   <td className={page.td}> {shiftHistory.employeeId} </td>
                   <td className={page.td}> {shiftHistory.employeeName} </td>
                   <td className={page.td}> {shiftHistory.unit} </td>
-                  <td className={page.td}>
-                    {shiftHistory.dateRequested}
-                  </td>
+                  <td className={page.td}>{shiftHistory.dateRequested}</td>
                   <td className={page.td}> {shiftHistory.shift} </td>
-                  <td className={page.td} style={statusStyle(shiftHistory.status)}>
+                  <td
+                    className={page.td}
+                    style={statusStyle(shiftHistory.status)}
+                  >
                     {" "}
                     {shiftHistory.status}{" "}
                   </td>
-                  {
-                    shiftHistory.status === "Pending" ? 
-                    (<td className={page.td}>
+                  {shiftHistory.status === "Pending" ? (
+                    <td className={page.td}>
                       <div className={page.buttons}>
-                        <button className={page.accept} onClick={() => { acceptOrDenyShift(shiftHistory.id, true) }}>Accept</button>
-                        <button className={page.deny} onClick={() => { acceptOrDenyShift(shiftHistory.id, false) }}>Deny</button>
+                        <button
+                          className={page.accept}
+                          onClick={() => {
+                            acceptOrDenyShift(shiftHistory.id, true);
+                          }}
+                        >
+                          Accept
+                        </button>
+                        <button
+                          className={page.deny}
+                          onClick={() => {
+                            acceptOrDenyShift(shiftHistory.id, false);
+                          }}
+                        >
+                          Deny
+                        </button>
                       </div>
-                    </td>) :
-                    (<td className={page.td}></td>)
-                  }
+                    </td>
+                  ) : (
+                    <td className={page.td}></td>
+                  )}
                 </tr>
               ))}
             </tbody>
