0d445b5c42bc1897463c89f415ff579a52172c3d
Jason Francois <jasonc.francois@gmail.com>
2024-05-13 12:26:43 -0400

    fixed mobile test bug

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,14 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      "babel-preset-expo",
-      "@babel/preset-env",
-      "@babel/preset-typescript",
-    ],
+    presets: ["babel-preset-expo"],
     plugins: [
       "expo-router/babel", // Required for expo-router
       "react-native-reanimated/plugin", // Required for Drawer component
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
     ],
   };
 };
